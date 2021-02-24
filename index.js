61748a5cf3381a3d96a818687c4d58823c10a1e7
DiaryKyosuke <fendianwar36@gmail.com>
2021-02-24 23:40:57 +0700

    create logs manual and convert to txt

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
-if (!process.argv[2]) {
-   return console.log('gagal')
-}
-console.log(process.argv[2])
+var fs = require('fs')
+
+fs.readFile('my-file.log',"utf-8",function(err, data){
+    console.log(data.split('\r\n'))
+    console.log(data.split('\r\n').length)
+    fs.writeFile("data.text", data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+})
+
+// if (!process.argv[2]) {
+//    return console.log('gagal')
+// }
+// console.log(process.argv[2])
