f99242a6b55b30d5ac34635305a712de58793c83
DiaryKyosuke <fendianwar36@gmail.com>
2021-02-25 10:39:28 +0700

    convert to json

--- index.js
+++ index.js
@@ -2,19 +2,39 @@
 var fs = require('fs')
 
 fs.readFile('my-file.log',"utf-8",function(err, data){
-    const cek = data.split('\r\n')
-    // console.log(Object.assign({}, cek))
-    // const json = JSON.stringify(Object.assign({}, cek), null, 2 )
-    // console.log(json)
-    fs.writeFile("data.json", data, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-})
+    let thisData = []
+    let array = data.split('\r\n')
+    for (let i = 0; i < array.length; i++) {
+        let cek = array[i].split('[')
+        for (let j = 1; j < cek.length; j++) {
+            let cekArray = cek[j].split(']')
+            const object = {
+                "date": cekArray[0],
+                "log": cekArray[1]
+            }
+            thisData.push(object)
 
-// if (!process.argv[2]) {
-//    return console.log('gagal')
-// }
-// console.log(process.argv[2])
+        }
+    }
+
+    const location = '/logs/data.json'
+    var lopping = location.split('/')
+    var directory = []
+    for (var i = 0; i < lopping.length-1; i++) {
+        directory.push(lopping[i])
+        
+    }
+    dir = '.'+directory.join("/")
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {
+            recursive: true
+        });
+    }
+       fs.writeFile('.'+location, JSON.stringify(thisData, null, 2), function (err) {
+           if (err) {
+               return console.log(err);
+           }
+           console.log("The file was saved!")
+       })
+
+})
