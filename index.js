ec0a348ab952346fdd0b0ca4f8a5e2454aa4901c
DiaryKyosuke <fendianwar36@gmail.com>
2021-02-25 12:56:18 +0700

    fix convert handle

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var fs = require('fs')
 
 
-if (!process.argv[2] || process.argv[2] === "-h") {
+if (!process.argv[2] || process.argv[2].split('.').includes('log') === false || process.argv[2] === "-h") {
     return console.log(`
     
     -h == help or for how to use the CLI
@@ -21,111 +21,123 @@ if (!process.argv[2] || process.argv[2] === "-h") {
     `)
 }
 
-if (process.argv[2]) {
-    console.log(process.argv)
+if (process.argv[2].split('.').includes('log')) {
     fs.readFile(process.argv[2],"utf-8",function(err, data){
         if (err) {
-            return console.log(err)
+            return console.log(`
+    
+    -h == help or for how to use the CLI
+    [location your file] -t json == convert .log to .json
+    example : converter /var/log/nginx/error.log -t json
+
+    [location your file] -t text == convert .log to .txt
+    example: converter /var/log/nginx/error.log -t text
+    
+    [location your file] -o [output directory file] == to convert files and set the output location
+    example: converter /var/log/nginx/error.log -o /User/johnmayer/Desktop/nginxlog.txt
+
+    [location your file] -t json/text -o [output location file] == to convert the file according to the command and  set the output location
+    example: converter /var/log/nginx/error.log -t json -o /User/johnmayer/Desktop/nginxlog.json
+    example: converter /var/log/nginx/error.log -t text -o /User/johnmayer/Desktop/nginxlog.txt
+    `)
             
         }
         // this convert to json
-        if (process.argv[3] === "-t" && process.argv[4] === "json") {
-           if(process.argv[5] === "-o" && process.argv[6]) {
-               let thisData = []
-               let array = data.split('\r\n')
-               for (let i = 0; i < array.length; i++) {
-                   let cek = array[i].split('[')
-                   for (let j = 1; j < cek.length; j++) {
-                       let cekArray = cek[j].split(']')
-                       const object = {
-                           "date": cekArray[0],
-                           "log": cekArray[1]
-                       }
-                       thisData.push(object)
+        if (process.argv[3] === "-t" && process.argv[4] === "json" && process.argv[5] === "-o" && process.argv[6].split('.').includes('json') === true) {
+            let thisData = []
+            let array = data.split('\r\n')
+            for (let i = 0; i < array.length; i++) {
+                let cek = array[i].split('[')
+                for (let j = 1; j < cek.length; j++) {
+                    let cekArray = cek[j].split(']')
+                    const object = {
+                        "date": cekArray[0],
+                        "log": cekArray[1]
+                    }
+                    thisData.push(object)
 
-                   }
-               }
+                }
+            }
 
-               const location = process.argv[6]
-               var lopping = location.split('/')
-               var directory = []
-               for (var i = 0; i < lopping.length - 1; i++) {
-                   directory.push(lopping[i])
+            const location = process.argv[6]
+            var lopping = location.split('/')
+            var directory = []
+            for (var i = 0; i < lopping.length - 1; i++) {
+                directory.push(lopping[i])
 
-               }
-               dir = '.' + directory.join("/")
-               if (!fs.existsSync(dir)) {
-                   fs.mkdirSync(dir, {
-                       recursive: true
-                   });
-               }
-               fs.writeFile('.' + location, JSON.stringify(thisData, null, 2), function (err) {
-                   if (err) {
-                       return console.log(err);
-                   }
-                   console.log("The file was convert to json!")
-               })
-           }else {
-               let thisData = []
-               let array = data.split('\r\n')
-               for (let i = 0; i < array.length; i++) {
-                   let cek = array[i].split('[')
-                   for (let j = 1; j < cek.length; j++) {
-                       let cekArray = cek[j].split(']')
-                       const object = {
-                           "date": cekArray[0],
-                           "log": cekArray[1]
-                       }
-                       thisData.push(object)
+            }
+            dir = '.' + directory.join("/")
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, {
+                    recursive: true
+                });
+            }
+            fs.writeFile('.' + location, JSON.stringify(thisData, null, 2), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was convert to json!")
+            })
+        }
+        else if (process.argv[3] === "-t" && process.argv[4] === "json" && !process.argv[5] && !process.argv[6]) {
+            let thisData = []
+            let array = data.split('\r\n')
+            for (let i = 0; i < array.length; i++) {
+                let cek = array[i].split('[')
+                for (let j = 1; j < cek.length; j++) {
+                    let cekArray = cek[j].split(']')
+                    const object = {
+                        "date": cekArray[0],
+                        "log": cekArray[1]
+                    }
+                    thisData.push(object)
 
-                   }
-               }
+                }
+            }
 
-               const location = '/logs-json/logs.json'
-               var lopping = location.split('/')
-               var directory = []
-               for (var i = 0; i < lopping.length - 1; i++) {
-                   directory.push(lopping[i])
+            const location = '/logs-json/logs.json'
+            var lopping = location.split('/')
+            var directory = []
+            for (var i = 0; i < lopping.length - 1; i++) {
+                directory.push(lopping[i])
 
-               }
-               dir = '.' + directory.join("/")
-               if (!fs.existsSync(dir)) {
-                   fs.mkdirSync(dir, {
-                       recursive: true
-                   });
-               }
-               fs.writeFile('.' + location, JSON.stringify(thisData, null, 2), function (err) {
-                   if (err) {
-                       return console.log(err);
-                   }
-                   console.log("The file was convert to json!")
-               })
-           }
+            }
+            dir = '.' + directory.join("/")
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, {
+                    recursive: true
+                });
+            }
+            fs.writeFile('.' + location, JSON.stringify(thisData, null, 2), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was convert to json!")
+            })
         }
         // convert to text
-        else if (process.argv[3] === "-t" && process.argv[4] === "text"){
-            if (process.argv[5] === "-o" && process.argv[6]) {
-                const location = process.argv[6]
-                var lopping = location.split('/')
-                var directory = []
-                for (var i = 0; i < lopping.length - 1; i++) {
-                    directory.push(lopping[i])
+        else if (process.argv[3] === "-t" && process.argv[4] === "text" && process.argv[5] === "-o" && process.argv[6].split('.').includes('txt') === true) {
+            const location = process.argv[6]
+            var lopping = location.split('/')
+            var directory = []
+            for (var i = 0; i < lopping.length - 1; i++) {
+                directory.push(lopping[i])
 
-                }
-                dir = '.' + directory.join("/")
-                if (!fs.existsSync(dir)) {
-                    fs.mkdirSync(dir, {
-                        recursive: true
-                    });
-                }
-                fs.writeFile('.' + location, data, function (err) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log("The file was convert to text!")
-                })
             }
-            else {
+            dir = '.' + directory.join("/")
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, {
+                    recursive: true
+                });
+            }
+            fs.writeFile('.' + location, data, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was convert to text!")
+            })
+        }
+        else if (process.argv[3] === "-t" && process.argv[4] === "text" && !process.argv[5] && !process.argv[6]){
                 const location = '/logs-text/logs.text'
                 var lopping = location.split('/')
                 var directory = []
@@ -145,10 +157,31 @@ if (process.argv[2]) {
                     }
                     console.log("The file was convert to text!")
                 })
-            }
         }
-        else if (process.argv[3] === "-o" && process.argv[4]) {
+        else if (process.argv[3] === "-o" && process.argv[4].split('.').includes('txt') === true) {
             const location = process.argv[4]
+            var lopping = location.split('/')
+            var directory = []
+            for (var i = 0; i < lopping.length - 1; i++) {
+                directory.push(lopping[i])
+
+            }
+            dir = '.' + directory.join("/")
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, {
+                    recursive: true
+                });
+            }
+            fs.writeFile('.' + location, data, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was convert to text!")
+            })
+        } 
+        //default convert to text .txt
+        else if (process.argv[2].split('.').includes('log') && !process.argv[3] && !process.argv[4] && !process.argv[5] && !process.argv[6]){
+            const location = '/logs-text/logs.text'
             var lopping = location.split('/')
             var directory = []
             for (var i = 0; i < lopping.length - 1; i++) {
